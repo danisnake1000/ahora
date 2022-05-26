18cc5ac69f79d88e57bb70543881c81477dab9f2
danisnake1000 <adriandanielfernandez@gmail.com>
2022-05-26 13:21:28 -0300

    hola

---
+++ app.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+//let nombre = prompt("ingrese su nombre: ");
+    //console.log(`Hola ${nombre}!!!`);
+
+    let queso = prompt("El valor es ");
+    let jamon = prompt("El valor es ");
+
+        console.log(`${queso} $100`);
+        console.log(`${jamon} $250`);
